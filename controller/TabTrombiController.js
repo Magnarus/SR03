84bfcef45c9231886238ac2ff351e3812a252f48
Vincent Keller <vincent-keller@hotmail.fr>
2016-04-01 22:20:03 +0200

    Change modal css / add select for page size

--- controller/TabTrombiController.js
+++ controller/TabTrombiController.js
@@ -13,12 +13,12 @@ trombiApp.controller('TabTrombiController', ['$scope',  'People', '$filter', '$m
 	
 	// Retourne les données qui correspondent à la chaine donnée en filtre
     $scope.getData = function () {
-      // https://docs.angularjs.org/api/ng/filter/filter
-      return $filter('filter')($scope.peopleData, $scope.filtre);
+       // https://docs.angularjs.org/api/ng/filter/filter
+       return $filter('filter')($scope.peopleData, $scope.filtre);
     }
 
 	// Calcul du nombre de page
-     $scope.numberOfPages=function(){
+     $scope.numberOfPages = function(){
         return Math.ceil($scope.getData().length/$scope.pageSize);                
     }
 
@@ -27,17 +27,23 @@ trombiApp.controller('TabTrombiController', ['$scope',  'People', '$filter', '$m
       $scope.pageSize = number;
     }
 	
+	// Fonction qui initialise les données du trombinoscope
 	$scope.init = function() {
 		$scope.peopleData = People;
 	};
 
+	
+	// Fenêtre ouverte avec des informations supplémentaires
 	$scope.showDialog = function($person) {
 		var parentEl = angular.element(document.body);
 		console.log($person);
 		$mdDialog.show({
 		 clickOutsideToClose: true,
          template:
-				'	<div class="mod"> <img src="https://demeter.utc.fr/portal/pls/portal30/portal30.get_photo_utilisateur_mini?username={{personne.login}}" onerror="this.src=\'resources/img/defaultImg.jpg\'"> </img>'
+				'  <div class="modal-dialog">'
+				+' <div class="modal-content">'
+				+ '	 <div class="modal-body">'
+				+ '<img src="https://demeter.utc.fr/portal/pls/portal30/portal30.get_photo_utilisateur_mini?username={{personne.login}}" onerror="this.src=\'resources/img/defaultImg.jpg\'"> </img>'
 				+ '	<ul class="list-unstyled">'
 				+ '	<li id="Name"> {{personne.nom}} </li>'
 				+ '	<li id="Login"> Login :  {{personne.login}} </li>'
@@ -47,7 +53,7 @@ trombiApp.controller('TabTrombiController', ['$scope',  'People', '$filter', '$m
 				+ '	<li><div id="Structure"> Structure : {{ personne.structure}} <b ng-if="!personne.structure">NA</b> </div> </li>'
 				+ '	<li><div id="Telephone"> Tél  : {{ personne.tel}} <b ng-if="!personne.tel">NA</b> </div> </li>'
 				+ '	<li><div id="Telephone2"> Tél 2 : {{ personne.tel2}} <b ng-if="!personne.tel2">NA</b> </div> </li>'
-				+ '	</ul></div>',
+				+ '	</ul></div></div></div></div>',
 		controller: function($scope, $mdDialog, personne){
 		   $scope.personne = personne;
 		},
